094e617e610c95724171d37da6b024a0e3c62008
LostLogia4 <hayate04@live.com>
2016-10-20 13:33:33 +0800

    Version update
    * Added support for link generation and hash predicates.
    * Original texts and hashed values are now in separate divs, to ease
    copy and paste.

--- code.js
+++ code.js
@@ -1,12 +1,23 @@
-var $embed = $("#embeds");
+var $embed1 = $("#embed1");
+var $embed2 = $("#embed2");
 document.getElementById("result").readOnly = true;
 
 function getHash() {
+	var salt = $("#salt").val();
 	var code = $("#input").val();
-	$("#result").val( md5(code) );
+	$("#result").val( md5(salt+code) );
 }
 
 function SaveHash() {
+	var prelink = $("#prelink").val();
+	var postlink = $("#postlink").val();
+	var salt = $("#salt").val();
 	var code = $("#input").val();
-	$embed.append(code+" "+md5(code)+"<br>");
+	$embed1.append(salt+code+"<br>");
+	$embed2.append(prelink+md5(salt+code)+postlink+"<br>");
 }
+
+$("#clearButton").click(function() {
+	$(".output").text('');
+	$("#result").val('');
+});
